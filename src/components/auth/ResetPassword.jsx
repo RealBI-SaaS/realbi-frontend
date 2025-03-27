8b6956f9b1e0f3b75a0545fc6a30cf7ee621bf92
Nathnael Yirga <Nathnaelyirga@gmail.com>
2025-03-27 17:10:19 +0300

    password reser fix, working

--- src/components/auth/ResetPassword.jsx
+++ src/components/auth/ResetPassword.jsx
@@ -36,34 +36,6 @@ const ResetPassword = () => {
 
   })
 
-  useEffect(() => {
-    const verifyEmail = async () => {
-      try {
-
-        const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/auth/users/activation/`,
-          //method: "POST",
-          {uid, token},
-          { headers: { "Content-Type": "application/json" }, }
-        );
-        //console.log(response)
-
-        if (response.status == '204') {
-          setMessage("Email verified successfully! Redirecting...");
-          //console.log("Account Verified")
-          setTimeout(() => navigate("/login"), 3000);
-        } else {
-          setMessage("Invalid or expired activation link.");
-        }
-      } catch (error) {
-        setMessage("An error occurred. Please try again.");
-      }
-    };
-
-    if (uid && token) {
-      verifyEmail();
-    }
-  }, [uid, token, navigate]);
-
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h2>{message}</h2>
