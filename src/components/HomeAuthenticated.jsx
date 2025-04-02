d760704118d7186fd168a98a96a7368d861853ed
Nathnael Yirga <Nathnaelyirga@gmail.com>
2025-04-02 17:22:09 +0300

    nav dropdown

--- src/components/HomeAuthenticated.jsx
+++ src/components/HomeAuthenticated.jsx
@@ -22,7 +22,7 @@ const HomeAuthenticated = () => {
     <div className="border flex justify-around items-center">
       <HomeOrgMenu onItemClick={handleItemClick} />{" "}
       {/* Pass the click handler */}
-      <div className="max-w-4xl mx-auto bg-green-500">
+      <div className="max-w-4xl mx-auto ">
         {/* Conditionally render content based on the selected item */}
         {selectedItem === "Welcome" ? (
           <div className="mb-6">
